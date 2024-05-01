8c24ea3984a7b3eef6b8534f5bf0f2117f277ab6
AhmedRashad654 <ahmedrashad6542@gmail.com>
2024-05-02 01:12:15 +0300

    first

---
+++ booklist.js
@@ -0,0 +1,35 @@
+let books = [
+  {
+    ISBN: 1,
+    category: "programming",
+    name: "oneBook",
+    image:
+      "https://img.freepik.com/free-photo/book-composition-with-open-book_23-2147690555.jpg",
+    numberOfPages: 200,
+    author: "ahmed",
+    price: 25,
+    isWatched: false,
+  },
+  {
+    ISBN: 2,
+    category: "programming",
+    name: "twoBook",
+    image:
+      "https://dictionary.cambridge.org/images/thumb/book_noun_001_01679.jpg?version=6.0.10",
+    numberOfPages: 400,
+    author: "ahmed",
+    price: 25,
+    isWatched: false,
+  },
+  {
+    ISBN: 3,
+    category: "programming",
+    name: "threeBook",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1A9dpvRJd6MK8qzMKUlNe246LDV_8_85UmtHE8prhwxHcMPlpTXUNvFBOnpPINHMEryI&usqp=CAU",
+    numberOfPages: 500,
+    author: "ahmed",
+    price: 25,
+    isWatched: false,
+  },
+];
